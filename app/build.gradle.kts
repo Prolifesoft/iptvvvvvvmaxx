c338a5739f19114bfa09c9528e87015892f029cc
Prolifesoft <ncem0332006@gmail.com>
2026-09-14 15:12:41 +0300

    build: bump version to 1.0.3
    Update versionCode to 103 and versionName to 1.0.3 for the new release.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
     applicationId = "com.maxxiptvplayer.pezvab"
     minSdk = 24
     targetSdk = 36
-    versionCode = 1
-    versionName = "1.0"
+    versionCode = 103
+    versionName = "1.0.3"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
